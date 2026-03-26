0601bfdca751b29d1de4cc63f192aff8f294a575
Beatriz Quaresma <beatriz.quaresma@icomp.ufam.edu.br>
2026-03-26 13:25:05 -0400

    Adicionando trabalho de AED2

---
+++ ex1.c
@@ -0,0 +1,11 @@
+/*
+Desenvolva um programa que implemente um vetor com 1.000.000 (um milhão) de
+elementos do tipo inteiro. Execute a busca sequencial no vetor com valores aleatórios
+desordenados e a busca binária no vetor com valores aleatórios ordenados. Execute cada
+algoritmo 30 vezes. Em 15 execuções uma posição do vetor deve ser sorteada para usar o
+valor correspondente naquela posição para a busca (isso garante que o valor vai ser
+encontrado no vetor) e nas outras 15 execuções gere valores aleatórios para a busca
+(muitos deles não serão encontrados no vetor). Calcule e mostre o tempo de execução para
+cada busca, a média entre as 30 execuções e o desvio padrão dos tempos de execução para
+cada algoritmo.
+*/
---
+++ ex3.c
@@ -0,0 +1,9 @@
+/*
+Desenvolva um programa que implementa um vetor com 100.000 (cem mil) elementos do
+tipo inteiro, com valores aleatórios desordenados. Ordene o vetor usando cinco técnicas:
+Bolha, Inserção Direta, Seleção Direta, Quicksort e Merge sort. Execute cada algoritmo de
+ordenação 10 vezes (10 rodadas de execução). Em cada rodada, use o mesmo vetor com os
+mesmos valores para cada uma das cinco técnicas de ordenação. Para a próxima rodada,
+gere novos valores para o vetor. Calcule e mostre o tempo para cada execução de cada
+algoritmo de ordenação e a média entre as 10 execuções para cada algoritmo.
+*/
