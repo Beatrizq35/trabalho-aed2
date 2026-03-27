9f69a3b2f33b24db9c7f8be88c7917413f69da57
Beatriz Quaresma <beatriz.quaresma@icomp.ufam.edu.br>
2026-03-27 10:52:05 -0400

    Atualizando pastas do trabalho de AED2

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,4 +8,36 @@ encontrado no vetor) e nas outras 15 execuções gere valores aleatórios para a
 (muitos deles não serão encontrados no vetor). Calcule e mostre o tempo de execução para
 cada busca, a média entre as 30 execuções e o desvio padrão dos tempos de execução para
 cada algoritmo.
+
+
+O q fazer:
+
+- Criar vetor do tipo inteiro
+- Implementar Busca Sequencial
+- Implementar Busca Binária
+- Criar programa de geracao de valores aleatorios
+- Criar programa que calcule o tempo de execucao
+- Criar programa que calcule a media entre as 30 execucoes
+- Criar programa que calcule o desvio padrao
 */
+
+#ind
+
+float desvio_padrao (int vet[], int n) {
+    int i;
+    float media, desvio;
+    float soma1, soma2, sub = 0.0;
+
+    //calcula a media
+    for (i = 0; i < n; i++) {
+        soma1 = soma1 + vet[i];
+    }
+    media = soma1 / n;
+
+    //subtrai a media de cada valor e eleva ao quadrado
+    for (i = 0; i < n; i++) {
+        sub = vet[i] - media;
+        soma2 = soma2 + (sub)
+    }
+
+}
