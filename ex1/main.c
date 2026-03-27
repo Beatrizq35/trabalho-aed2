6581e775ea5cfddc4ffd715e7ee41933e2491b04
Ruthelene <ruthelene.farias@gmail.com>
2026-03-27 15:08:25 -0400

    Update main.c

--- ex1/main.c
+++ ex1/main.c
@@ -1,1 +1,21 @@
-Onde fazemos os testes, calculamos o tempo e exibimos os resultados.
+// Chamada das funções
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "EX1.h"
+
+
+int main(){
+  void criaVetDesordenado(vet[],tam);
+  void criaVetOrdenado(vet[],tam);
+  int buscaSequencial (vet[],key);
+  int buscaBinaria(vet[],size,chave);
+  int geraValorAleatorio(vet[],tam,garantirEncontrado);
+  double calculaTempo(struct timespec inicio, struct timespec fim);
+  double calculaMedia(double tempos[], int n);
+  double desvioPadrao(double tempos[], int n);
+  
+
+  
+}
+
