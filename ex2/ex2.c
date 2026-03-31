be39e4990441394746d5817af878dcc0143e5d9c
Ruthelene <ruthelene.farias@gmail.com>
2026-03-31 13:20:13 -0400

    Delete ex2 directory

--- ex2/ex2.c
+++
@@ -1,9 +0,0 @@
-/*
-Aproveitando o vetor da questão anterior utilizado na busca sequencial, crie uma lista
-encadeada com os mesmos valores. Execute a busca sequencial 30 vezes no vetor e 30
-vezes na lista encadeada, usando os mesmos valores para a busca em cada execução nas
-duas estruturas de dados. Registre os tempos de execução de cada busca, calcule e exiba
-a média dos tempos de execução para cada estrutura. Use o mesmo mecanismo da questão
-anterior para garantir que pelo menos 15 valores sejam encontrados nas estruturas de
-dados.
-*/
